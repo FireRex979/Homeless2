0df1a1f7c95baf1e6aa8c3e885f8c2174a443e45
FireRex979 <aldiwahyudi@student.unud.ac.id>
2020-07-09 13:22:03 +0800

    Merge Admin User

--- resources/views/user/furniture.blade.php
+++ resources/views/user/furniture.blade.php
@@ -11,128 +11,151 @@
         $('#furniture').addClass('act-link');
     });
 </script>
- <!-- wrapper -->	
+<!-- wrapper -->    
             <div id="wrapper">
                 <!--  content  --> 
                 <div class="content">
-                    <!--  section  end--> 
-                    <!--  section  --> 
-                    <section class="parallax-section" data-scrollax-parent="true">
-                        <div class="bg par-elem "  data-bg="assets/user/images/bg/1.jpg" data-scrollax="properties: { translateY: '30%' }"></div>
-                        <div class="overlay"></div>
-                        <div class="container">
-                            <div class="section-title center-align">
-                                <h2><span>Furniture</span></h2>
-                                <div class="breadcrumbs fl-wrap"><a href="#">Beranda</a><span>Furniture</span></div>
-                                <span class="section-separator"></span>
-                            </div>
-                        </div>
-                        <div class="header-sec-link">
-                            <div class="container"><a href="#sec1" class="custom-scroll-link">Let's Start</a></div>
-                        </div>
-                    </section>
-                    <!--  section  end--> 
                     <!--  section  --> 
                     <section class="gray-bg no-pading no-top-padding" id="sec1">
-                        <div class="col-list-wrap fh-col-list-wrap  left-list">
+                        <div class="col-list-wrap  center-col-list-wrap left-list">
                             <div class="container">
-                                <div class="row">
-                                    <div class="col-md-8">
-                                        <div class="listsearch-header fl-wrap">
-                                            <h3>Results For : <span>Semua</span></h3>
-                                            <div class="listing-view-layout">
-                                                <ul>
-                                                    <li><a class="grid active" href="#"><i class="fa fa-th-large"></i></a></li>
-                                                    <li><a class="list" href="#"><i class="fa fa-list-ul"></i></a></li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                        <!-- list-main-wrap-->
-                                        <div class="list-main-wrap fl-wrap card-listing">
-                                            <!-- listing-item -->
-                                            @for($i=1;$i<=3;$i++)
-                                            <div class="listing-item">
-                                                <article class="geodir-category-listing fl-wrap">
-                                                    <div class="geodir-category-img">
-                                                        <img src="assets/user/images/all/1.jpg" alt="">
-                                                        <div class="overlay"></div>
-                                                    </div>
-                                                    <div class="geodir-category-content fl-wrap">
-                                                        <a class="listing-geodir-category" href="listing.html">Cek</a>
-                                                        <h3><a href="listing-single.html">Furniture {{$i}}</a></h3>
-                                                        <p>Sed interdum metus at nisi tempor laoreet. Integer gravida orci a justo sodales, sed lobortis est placerat.</p>
-                                                        <div class="geodir-category-options fl-wrap">
-                                                            <div class="geodir-category-location"><a href="#"><i class="fa fa-money-bill-wave" aria-hidden="true"></i> Rp. 10000000</a></div>
-                                                        </div>
-                                                    </div>
-                                                </article>
-                                            </div>
-                                            <!-- listing-item end-->                           
-                                            <!-- listing-item -->
-                                            <div class="listing-item">
-                                                <article class="geodir-category-listing fl-wrap">
-                                                    <div class="geodir-category-img">
-                                                        <img src="assets/user/images/all/1.jpg" alt="">
-                                                        <div class="overlay"></div>
-                                                    </div>
-                                                    <div class="geodir-category-content fl-wrap">
-                                                        <a class="listing-geodir-category" href="listing.html">Cek</a>
-                                                        <h3><a href="listing-single.html">Furniture {{$i+1}}</a></h3>
-                                                        <p>Morbi suscipit erat in diam bibendum rutrum in nisl. Aliquam et purus ante.</p>
-                                                        <div class="geodir-category-options fl-wrap">
-                                                            <div class="geodir-category-location"><a href="#"><i class="fa fa-money-bill-wave" aria-hidden="true"></i> Rp. 10000000</a></div>
-                                                        </div>
-                                                    </div>
-                                                </article>
-                                            </div>
-                                            <!-- listing-item end-->  
-                                            <div class="clearfix"></div>
-                                            @endfor
-                                            <!-- listing-item end-->   
-                                            <!-- pagination-->
-                                            <div class="pagination">
-                                                <a href="#" class="prevposts-link"><i class="fa fa-caret-left"></i></a>
-                                                <a href="#" class="blog-page transition">1</a>
-                                                <a href="#" class="blog-page current-page transition">2</a>
-                                                <a href="#" class="blog-page transition">3</a>
-                                                <a href="#" class="blog-page transition">4</a>
-                                                <a href="#" class="nextposts-link"><i class="fa fa-caret-right"></i></a>
-                                            </div>
-                                        </div>
-                                        <!-- list-main-wrap end-->                           
+                                <div class="listsearch-maiwrap box-inside fl-wrap">
+                                    <div class="listsearch-header fl-wrap">
+                                        <h3>Furniture <span>dan</span> Paket Furniture</h3>
                                     </div>
-                                    <div class="col-md-4">
-                                        <div class="fl-wrap">
-                                            <!-- listsearch-input-wrap  -->  
-                                            <div class="listsearch-input-wrap fl-wrap">
-                                                <div class="listsearch-input-item">
-                                                    <i class="mbri-key single-i"></i>
-                                                    <input type="text" placeholder="Cari Sesuatu?" value=""/>
-                                                </div>
-                                                <div class="listsearch-input-item">
-                                                    <select data-placeholder="All Categories" class="chosen-select" >
-                                                        <option>Semua Kategori</option>
-                                                        <option>Shops</option>
-                                                        <option>Hotels</option>
-                                                        <option>Restaurants</option>
-                                                        <option>Fitness</option>
-                                                        <option>Events</option>
-                                                    </select>
-                                                </div>
-                                                <!-- hidden-listing-filter end -->
-                                                <button class="button fs-map-btn">Cari</button>
-                                            </div>
-                                            <!-- listsearch-input-wrap end -->
+                                    <!-- listsearch-input-wrap  -->  
+                                    <div class="listsearch-input-wrap fl-wrap">
+                                        <div class="listsearch-input-item">
+                                            <i class="mbri-key single-i"></i>
+                                            <input type="text" placeholder="Cari Sesuatu?" value=""/>
                                         </div>
+                                        <div class="listsearch-input-item">
+                                            <select data-placeholder="Location" class="chosen-select" name="category_properti">
+                                                <option value="semua">Semua</option>
+                                                <option value="paket">Paket</option>
+                                                <option value="furniture">Furniture</option>
+                                            </select>
+                                        </div>
+                                        <div class="listsearch-input-item" id="test">
+                                            <select class="chosen-select" name="category_detail">
+                                                <option>Pilih Kategori Properti Dahulu</option>
+                                            </select>
+                                        </div>
+                                        <!-- hidden-listing-filter -->
+                                        <div class="hidden-listing-filter fl-wrap" style="margin-top: 20px;">
+                                            <div class="listsearch-input-item">
+                                                <select data-placeholder="Location" class="chosen-select" name="category_properti">
+                                                    <option value="semua">Minimal Harga</option>
+                                                    <option value="rumah">10 juta</option>
+                                                    <option value="tanah">50 juta</option>
+                                                    <option value="tanah">100 juta</option>
+                                                    <option value="tanah">150 juta</option>
+                                                </select>
+                                            </div>
+                                            <div class="listsearch-input-item">
+                                                <select data-placeholder="Location" class="chosen-select" name="category_properti">
+                                                    <option value="semua">Maksimal Harga</option>
+                                                    <option value="rumah">10 juta</option>
+                                                    <option value="tanah">50 juta</option>
+                                                    <option value="tanah">100 juta</option>
+                                                    <option value="tanah">150 juta</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <!-- hidden-listing-filter end -->
+                                        <button class="button fs-map-btn" style="margin-top: 10px;">Update</button>
+                                        <div class="more-filter-option">More Filters <span></span></div>
+                                    </div>
+                                    <!-- listsearch-input-wrap end -->   
+                                </div>
+                                <!-- list-main-wrap-->
+                                 <!--section -->
+                    <section class="gray-section">
+                        <div class="container">
+                            <div class="section-title">
+                                <h2>List Paket Furniture</h2>
+                                <span class="section-separator"></span>
+                                <p>Dapatkan Paket Furniture yang lebih murah!</p>
+                            </div>
+                        </div>
+                        <!-- carousel -->
+                        <div class="list-carousel fl-wrap card-listing ">
+                            <!--listing-carousel-->
+                            <div class="listing-carousel  fl-wrap ">
+                                <!--slick-slide-item-->
+                                @for($i=1;$i<=9;$i++)
+                                    <!-- listing-item -->
+                                    <div class="listing-item">
+                                        <article class="geodir-category-listing fl-wrap">
+                                            <div class="geodir-category-img">
+                                                <img src="assets/user/images/all/1.jpg" alt="">
+                                                <div class="overlay"></div>
+                                            </div>
+                                            <div class="geodir-category-content fl-wrap">
+                                                <a class="listing-geodir-category" href="/residence-detail">Lihat</a>
+                                                <h3><a href="/furniture-detail">Paket Furniture {{$i}}</a></h3>
+                                                <p>Deskripsi Paket Furniture</p>
+                                                <div class="geodir-category-options fl-wrap">
+                                                    <div class="geodir-category-location"><p > Rp. {{$i * 1000000}}</p></div>
+                                                </div>
+                                            </div>
+                                        </article>
+                                    </div>
+                                    @endfor
+                            </div>
+                            <!--listing-carousel end-->
+                            <div class="swiper-button-prev sw-btn"><i class="fa fa-long-arrow-left"></i></div>
+                            <div class="swiper-button-next sw-btn"><i class="fa fa-long-arrow-right"></i></div>
+                        </div>
+                        <!--  carousel end-->
+                    </section>
+                    <!-- section end -->
+                                <div class="list-main-wrap fl-wrap card-listing">
+                                    <div class="section-title">
+                                        <h2>List Furniture</h2>
+                                        <span class="section-separator"></span>
+                                        <p>Dapatkan Furniture yang Berkelas!</p>
+                                    </div>
+                                    @for($i=1;$i<=9;$i++)
+                                    <!-- listing-item -->
+                                    <div class="listing-item">
+                                        <article class="geodir-category-listing fl-wrap">
+                                            <div class="geodir-category-img">
+                                                <img src="assets/user/images/all/1.jpg" alt="">
+                                                <div class="overlay"></div>
+                                            </div>
+                                            <div class="geodir-category-content fl-wrap">
+                                                <a class="listing-geodir-category" href="/residence-detail">Lihat</a>
+                                                <h3><a href="/furniture-detail">Furniture {{$i}}</a></h3>
+                                                <p>Deskripsi Furniture</p>
+                                                <div class="geodir-category-options fl-wrap">
+                                                    <div class="geodir-category-location"><p > Rp. {{$i * 1000000}}</p></div>
+                                                </div>
+                                            </div>
+                                        </article>
+                                    </div>
+                                    @if($i%3==0)
+                                        <div class="clearfix"></div>  
+                                    @endif
+                                    <!-- listing-item end-->
+                                    @endfor                                                                           
+                                    <!-- pagination-->
+                                    <div class="pagination">
+                                        <a href="#" class="prevposts-link"><i class="fa fa-caret-left"></i></a>
+                                        <a href="#" class="blog-page current-page transition">1</a>
+                                        <a href="#" class="blog-page transition">2</a>
+                                        <a href="#" class="blog-page transition">3</a>
+                                        <a href="#" class="blog-page transition">4</a>
+                                        <a href="#" class="nextposts-link"><i class="fa fa-caret-right"></i></a>
                                     </div>
                                 </div>
+                                <!-- list-main-wrap end-->                           
                             </div>
                         </div>
                     </section>
                     <!--  section  end--> 
-                    <div class="limit-box fl-wrap"></div>
                 </div>
-                <!-- content end--> 
+                <!--  content  end--> 
             </div>
             <!-- wrapper end -->
 @endsection
